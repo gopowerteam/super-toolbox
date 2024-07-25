8089766f92a9fdeb45ad0f7bf3a3ab1975f991f1
zhuchentong <2037630@163.com>
2024-07-25 11:00:43 +0800

    update

---
+++ src/bootstrap/launch/app.launch.ts
@@ -0,0 +1,46 @@
+function checkAppUpdate() {
+  const updateManager = uni.getUpdateManager()
+
+  updateManager.onUpdateReady(() => {
+    uni.showModal({
+      title: '更新提示',
+      content: '新版本已经准备好，是否重启应用？',
+      success: ({ confirm }) => confirm && updateManager.applyUpdate(),
+    })
+  })
+
+  updateManager.onUpdateFailed(() => {
+    // 新的版本下载失败
+  })
+
+  updateManager.onCheckForUpdate(({ hasUpdate }) => {
+    // 请求完新版本信息的回调
+    if (hasUpdate) {
+      uni.showToast({
+        title: '检测到新版本,正在准备更新',
+        mask: true,
+      })
+    }
+  })
+}
+
+function setupAppShare() {
+  // #ifdef MP-WEIXIN
+  const descriptor = Object.getOwnPropertyDescriptor(wx, 'onAppRoute')
+  const onAppRoute = descriptor?.value
+
+  if (onAppRoute) {
+    onAppRoute(() => {
+      uni.showShareMenu({
+        withShareTicket: true,
+        menus: ['shareAppMessage', 'shareTimeline'],
+      })
+    })
+  }
+  // #endif
+}
+
+export function appLaunch() {
+  checkAppUpdate()
+  setupAppShare()
+}
--- src/bootstrap/launch/index.ts
+++ src/bootstrap/launch/index.ts
@@ -1,4 +1,11 @@
+import { appLaunch } from './app.launch'
+import { userLaunch } from './user.launch'
+
 export async function launch() {
   const store = useStore()
+
+  await appLaunch()
+  await userLaunch()
+
   store.app.setReady()
 }
